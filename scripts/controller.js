1aab91ffba7002bc2683dc69c8e4a8ca5320711a
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-10-09 15:36:38 +0800

    Host trying

--- scripts/controller.js
+++ scripts/controller.js
@@ -3,13 +3,12 @@ $(document).ready(function (e) {
     var brokweAdd = "wss://test.mosquitto.org:8081/mqtt";
     var time = new Date($.now());
     client = mqtt.connect(brokweAdd);
-
     client.on("connect", function () {
-        client.subscribe('jane/fan/status')
         $(document).on('click', 'button', function () {
             var click = $(this).attr("id")
             $('h2').empty();
             $('h2').append(click);
+            client.subscribe('jane/fan/status')
             client.publish('jane/fan/status', "Turned at " + click + " : " + time)
         })
     })
